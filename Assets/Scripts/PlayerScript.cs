e2b948d6464f651fb2479b3b81f776fc3dfe5c43
91230jack@gmail.com <91230jack@gmail.com>
2020-10-25 22:32:37 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,7 +8,13 @@ public class PlayerScript : MonoBehaviour
 
     public float speed;
 
+    public Animator animator;
+
+    private bool facingRight = true;
+
     public Text score;
+
+    public Text winText;
 
     private int scoreValue = 0;
 
@@ -17,6 +23,7 @@ public class PlayerScript : MonoBehaviour
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        winText.text = "";
     }
 
     // Update is called once per frame
@@ -25,11 +32,33 @@ public class PlayerScript : MonoBehaviour
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+
+        animator.SetFloat("Speed", Mathf.Abs (hozMovement));
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+
         if (Input.GetKey("escape"))
         {
             Application.Quit();
         }
     }
+
+
+    void Flip()
+   {
+     facingRight = !facingRight;
+     Vector2 Scaler = transform.localScale;
+     Scaler.x = Scaler.x * -1;
+     transform.localScale = Scaler;
+   }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
        if (collision.collider.tag == "Coin")
@@ -37,17 +66,25 @@ public class PlayerScript : MonoBehaviour
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+
+            if (scoreValue == 4)
+            {
+                winText.text = "Nice Work!!! made by Jack Ni";
+            }
         }
 
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
+         animator.SetBool("IsJumping", false);
+
         if (collision.collider.tag == "Ground")
         {
             if (Input.GetKey(KeyCode.W))
             {
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                animator.SetBool("IsJumping", true);
             }
         }
     }
